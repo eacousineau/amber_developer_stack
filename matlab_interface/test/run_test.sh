cf69e1134b6136613fdf547353102482546ffa91
eacousineau <eacousineau@gmail.com>
2014-04-13 10:43:25 -0500

    matlab_interface: Add README, robustify test running

--- matlab_interface/test/run_test.sh
+++ matlab_interface/test/run_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd "$(dirname "$BASH_SOURCE")"
-../scripts/matlab_console.sh -r 'test(); exit();'
+../scripts/matlab_console.sh -r "try; test(); catch e; disp(getReport(e, 'extended')); end; exit();"
--- matlab_interface/test/test.m
+++ matlab_interface/test/test.m
@@ -4,7 +4,8 @@
 % @note You must ensure that it is built, and that you've sourced the appropriate
 % `devel/setup.bash`!
 name = 'matlab_interface_test';
-[~, lib] = system(sprintf('catkin_find --first-only %s.%s', name, mexext()));
+[status, lib] = system(sprintf('catkin_find --first-only %s.%s', name, mexext()));
+assert(status == 0, 'Could not find mex library: %s', name);
 lib_path = fileparts(lib);
 addpath(lib_path);
 
