e07c23c9c994765bd206c245086b3415f0424111
Eric Cousineau <eacousineau@gmail.com>
2014-06-11 09:07:00 -0500

    run_tests: Robustify against build errors

--- eigen_utilities/run_tests.sh
+++ eigen_utilities/run_tests.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-name="$(basename "$(cd "$(dirname "$BASH_SOURCE")" && pwd)")"
+pkg="$(cd "$(dirname "$BASH_SOURCE")/.." && pwd)"
+cd $pkg
+
+name="$(basename $pkg)"
 log_dir="/tmp/$name/log"
+
 bin="./devel/lib/$name/$name-test"
 
 mkdir -p $log_dir
@@ -24,19 +28,25 @@ test-target-low()
     {
         cmake ../.. -DCMAKE_BUILD_TYPE=${type} $flags
         make -j4 all && make -j4 tests
+    } > "$log" 2>&1 || { echo "[ build error: ${label} ]"; cat $log >&2; false; }
+    {
         $bin && pass="true" || pass="false"
-    } > "$log" 2>&1
+    } >> "$log" 2>&1
     echo -e "-   label: ${label}\n    type: ${type}\n    flags: ${flags}\n    pass: $pass\n    log: $log"
 ) }
 
 test-target()
 {
+    (
+    set -e -u
     type=$1
     test-target-low "type-${type}" "${type}" ""
     test-target-low "type-${type}-extern" "${type}" "-DEIGEN_UTILITIES_TEST_EXTERN=ON"
     test-target-low "type-${type}-ndebug" "${type}" "-DEIGEN_UTILITIES_NDEBUG=ON"
+    )
 }
 
+set -e -u
 echo "results:"
 test-target Release
 test-target MinSizeRel 
