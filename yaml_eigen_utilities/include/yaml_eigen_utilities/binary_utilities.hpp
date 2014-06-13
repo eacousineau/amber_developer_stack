60e0b2425b5492243789ff12aba21b962cbe551c
Eric Cousineau <eacousineau@gmail.com>
2014-06-12 20:42:33 -0500

    binary_utilities: Use template specialization

--- yaml_eigen_utilities/include/yaml_eigen_utilities/binary_utilities.hpp
+++ yaml_eigen_utilities/include/yaml_eigen_utilities/binary_utilities.hpp
@@ -12,6 +12,7 @@
 namespace yaml_utilities
 {
 
+    template<>
 inline void yaml_write_binary(YAML::Emitter &out, const Eigen::VectorXd &X)
 {
     out << YAML::Flow << YAML::BeginSeq;
@@ -20,29 +21,13 @@ inline void yaml_write_binary(YAML::Emitter &out, const Eigen::VectorXd &X)
     out << YAML::EndSeq;
 }
 
+    template<>
 inline void yaml_read_binary(const YAML::Node &out, Eigen::VectorXd &X)
 {
     int rows = out.size();
     X.resize(rows);
     for (int i = 0; i < rows; ++i)
         yaml_read_binary(out[i], X(i));
-}
-
-/// @note Copy of template, but specializing. Must do this to ensure that Eigen::VectorXd is called
-inline void yaml_write_binary_dual(YAML::Emitter &out, const Eigen::VectorXd &X)
-{
-    // Write a tuple of dual-representation
-    out << YAML::BeginMap
-        << YAML::Key << "value" << YAML::Value << X
-        << YAML::Key << "base64" << YAML::Value;
-    yaml_write_binary(out, X);
-    out << YAML::EndMap;
-}
-
-inline void yaml_read_binary_dual(const YAML::Node &node, Eigen::VectorXd &X)
-{
-    // Read the binary base64 portion
-    yaml_read_binary(node["base64"], X);
 }
 
 }
