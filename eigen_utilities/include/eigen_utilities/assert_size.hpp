4babcbe333a86a401b79376db2121c4a862caa6a
Eric Cousineau <eacousineau@gmail.com>
2015-01-16 11:45:10 -0800

    eigen_utilities: Add macros for ease of use with assert_size

--- eigen_utilities/include/eigen_utilities/assert_size.hpp
+++ eigen_utilities/include/eigen_utilities/assert_size.hpp
@@ -15,6 +15,26 @@
 namespace eigen_utilities
 {
 
+#define assert_size_matrix(X, rows_expected, cols_expected) \
+    { \
+        common_assert_msg_ex( \
+            X.rows() == rows_expected && X.cols() == cols_expected, \
+            "matrix [row, col] mismatch" << std::endl << \
+                "actual: [" << X.rows() << ", " << X.cols() << "]" << std::endl << \
+                "expected: [" << rows_expected << ", " << cols_expected << "]", \
+            eigen_utilities::assert_error); \
+    }
+
+#define assert_size_vector(X, size_expected) \
+    { \
+        common_assert_msg_ex( \
+            X.size() == size_expected, \
+            "matrix size mismatch" << std::endl << \
+                "actual: " << X.size() << std::endl << \
+                "expected: " << size_expected, \
+            eigen_utilities::assert_error); \
+    }
+
     template<typename Derived>
 void assert_size(const Eigen::MatrixBase<Derived> &X, int rows_expected, int cols_expected)
 {
