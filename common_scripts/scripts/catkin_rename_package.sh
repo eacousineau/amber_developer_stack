d6223502f75ef8441b7595b1335687e5f5305d68
Eric Cousineau <eric.cousineau@tri.global>
2017-06-17 23:50:37 -0400

    Minor tweaks

--- common_scripts/scripts/catkin_rename_package.sh
+++ common_scripts/scripts/catkin_rename_package.sh
@@ -16,6 +16,7 @@ these changes!"
 OPTIONS_SPEC="\
 usage: $bin [options] <dir> <from> <to>
 
+	-p, --pause  Pause to show renames
     -h, --help   Show help
 
 $LONG_USAGE
@@ -27,11 +28,22 @@ usage()
 	exit ${1-0}
 }
 
+pause=
+do-pause() {
+	if [[ -n "$pause" ]]; then
+		echo "Press ENTER..."
+		read
+	fi
+}
+
 while [[ $# -gt 0 ]]
 do
 	case "$1" in
 	-h|--help)
 		usage
+		;;
+	-p|--pause)
+		pause=1
 		;;
 	-*)
 		echo "error: Invalid option: $1"
@@ -58,12 +70,13 @@ to="$3"
 
 # File type / name flags - implementing implode functionality :/
 # python -c 'print " -o ".join(map(lambda x: "-name " + x, ["*.cpp", "*.hpp"]))'
-patterns='*.cpp *.hpp *.h *.c *.m *.py package.xml CMakeLists.txt *.cmake *.sh'
-name_flags=""
-for pattern in $patterns
+patterns=('*.cpp' '*.hpp' '*.cc' '*.h' '*.c' '*.m' '*.py' 'package.xml' 'CMakeLists.txt' '*.cmake' '*.sh' 'BUILD' '*.bzl')
+name_flags=()
+for pattern in "${patterns[@]}"
 do
-	flag="-name $pattern"
-	[[ -z "$name_flags" ]] && name_flags="$flag" || name_flags="$name_flags -o $flag"
+	flag=(-name "$pattern")
+	echo "${flag[@]}"
+	[[ ${#name_flags[@]} -eq 0 ]] && name_flags=("${flag[@]}") || name_flags=("${name_flags[@]}" -o "${flag[@]}")
 done
 
 # Prune flags
@@ -75,19 +88,33 @@ do
 	[[ -z "$prune_flags" ]] && prune_flags="$flag" || prune_flags="$prune_flags -o $flag"
 done
 
+regex_flags=-regex\ ".*[\b_]?$from[\b_]?.*"
+
 echo "[ Rename Package: '$from' -> '$to' ]"
 # http://stackoverflow.com/questions/4210042/exclude-directory-from-find-command
 # @note This works with -prune because our names are typically files types
 # @todo http://stackoverflow.com/questions/8677546/bash-for-in-looping-on-null-delimited-string-variable
-files="$(find "$dir" $prune_flags -o \( $name_flags \) -print)"
+files="$(find "$dir" $prune_flags -o \( "${name_flags[@]}" \) -print)"
 echo "[ Text Replacement for Patterns: $patterns ]"
+echo $files
+do-pause
 sed -i "s#\b$from\b#$to#g" $files
 
 # Next rename directories
 echo "[ Rename Directories ]"
 srcs="$(find "$dir" -name "$from" -type d | tac)"
+echo $srcs
+do-pause
 for src in $srcs
 do
 	dest="$(dirname $src)/$to"
 	mv "$src" "$dest"
+done
+
+echo "[ Rename Files ]"
+srcs="$(find "$dir" $regex_flags -type f)"
+echo $srcs
+do-pause
+for src in $srcs; do
+	rename "s#([\b_]?)${from}([\b_]?)#\$1${to}\$2#g" $src
 done
